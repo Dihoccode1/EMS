c2761b76bc90c075c23d5c95413bc8678724c0a5
Dihoccode1” <khdidenroiday@gmail.com>
2026-06-27 11:56:54 +0700

    connected to database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 // server.js
 import express from "express";
 import cors from "cors";
-import "dotenv/config"; // Đã xóa 3 khoảng trắng thừa ở đây
+import "dotenv/config";
 import multer from "multer";
+import connectDB from "./config/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const HOSTNAME = process.env.HOSTNAME || "localhost";
 
 // Middleware
 app.use(cors());
@@ -14,9 +16,11 @@ app.use(multer().none());
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("Server is running");
+  res.send(`Server is running at ${HOSTNAME}:${PORT}`);
 });
-
+// Connect to MongoDB
+connectDB();
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
