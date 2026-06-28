6d4c1123b67f09b96310e7604803035eaae49a35
Khanhduy <khdidenroiday@gmail.com>
2026-06-28 22:57:12 +0700

    completed Dashboard controller

--- server/models/Attendance.js
+++ server/models/Attendance.js
@@ -1,30 +1,27 @@
 import mongoose from "mongoose";
 
-const attendanceSchema = new mongoose.Schema(
-  {
+const attendanceSchema = new mongoose.Schema({
     employeeID: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Employee",
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Employee",
     },
     date: { type: Date, required: true },
     checkIn: { type: Date, default: null },
     checkOut: { type: Date, default: null },
     status: {
-      type: String,
-      enum: ["PRESENT", "ASCENT", "LATE"],
-      default: "PRESENT",
-      workingHours: { type: Number, default: null },
-      dayType: {
         type: String,
-        enum: ["Full Day", "Three Quarter Day", "Half Day", "Short Day", null],
-        default: null,
-      },
+        enum: ["PRESENT", "ASCENT", "LATE"],
+        default: "PRESENT",
+        workingHours: { type: Number, default: null },
+        dayType: {
+            type: String,
+            enum: ["Full Day", "Three Quarter Day", "Half Day", "Short Day", null],
+            default: null,
+        },
     },
-  },
-  { timestamps: true },
-);
+}, { timestamps: true }, );
 attendanceSchema.index({ employeeID: 1, date: 1 }, { unique: true });
 const Attendance =
-  mongoose.model.Attendance || mongoose.model("Attendance", userSchema);
+    mongoose.model.Attendance || mongoose.model("Attendance", attendanceSchema);
 
 export default Attendance;
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ import profileRouter from "./routes/profileRoutes.js";
 import attendanceRouter from "./routes/attendanceRoutes.js";
 import leaveRouter from "./routes/leaveRoutes.js";
 import payslipRouter from "./routes/payslipRoutes.js";
+import dashboardRouter from "./routes/dashboardRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -30,6 +31,7 @@ app.use("/api/profile", profileRouter);
 app.use("/api/attendance", attendanceRouter);
 app.use("/api/leave", leaveRouter);
 app.use("/api/payslip", payslipRouter);
+app.use("/api/dashboard", dashboardRouter);
 // Connect to MongoDB
 connectDB();
 // Start the server
